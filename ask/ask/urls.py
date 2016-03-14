e8af4d25c3895e3c37780b445e1b0670b9191e1f
floud <ahalturin84@gmail.com>
2016-03-14 22:20:33 +0300

    first django

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from qa.views import test
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +9,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', test, name='home'),
+	url(r'^login/', test, name='login'),
+	url(r'^signup/', test, name='signup'),
+	url(r'^question/\d+', test, name='question'),
+	url(r'^ask/', test, name='ask'),
+	url(r'^popular/', test, name='popular'),
+	url(r'^new/', test, name='new'),
 )
