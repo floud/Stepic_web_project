6d160a90d50a2fd120b4f924acd0e9bca1ae0271
floud <ahalturin84@gmail.com>
2016-03-15 13:51:12 +0300

    test1

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/test
 cd /home/box/web/ask
 sudo gunicorn -c /etc/gunicorn.d/test ask.wsgi:application &
+
+sudo mysqld_safe --user=mysql &
+mysql -uroot -e "create database defaultdb;"
+mysql -uroot -e "CREATE USER 'floud' IDENTIFIED BY 'square';"
