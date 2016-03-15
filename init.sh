c1e35e46b275547634a8256569ac55dbe0c808b0
floud <ahalturin84@gmail.com>
2016-03-15 13:15:35 +0300

    test

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default1
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+cd /home/box/web/ask
+sudo gunicorn -c /etc/gunicorn.d/test ask.wsgi:application &
