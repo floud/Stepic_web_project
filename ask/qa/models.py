b76f6da2698a20135245dca2fe83ddbc80e6417d
floud <ahalturin84@gmail.com>
2016-03-21 13:02:49 +0300

    models.py2

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here
 
 class Question(models.Model):
@@ -11,9 +12,9 @@ class Question(models.Model):
     author = User()
     likes = [User]
 
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True)
-    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE())
+    question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
     author = User()
-
