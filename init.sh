413afc055a637f2e18a67364360bab435839620d
floud <ahalturin84@gmail.com>
2016-05-24 12:45:27 +0300

    test

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default1
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/test
-cd /home/box/web/ask/ask
-sudo gunicorn -c /etc/gunicorn.d/test wsgi:application &
+cd /home/box/web/ask/ask/
+sudo gunicorn -c /home/box/web/etc/hello.py wsgi:application &
 
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database defaultdb;"
