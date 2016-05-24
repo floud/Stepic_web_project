8776add6554da7059ad5c00d1a3652c4247e98a0
floud <ahalturin84@gmail.com>
2016-05-24 09:39:49 +0300

    models 3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,17 +4,20 @@ from django.contrib.auth.models import User
 
 # Create your models here
 
+class DjangoUser(User):
+    pass
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True)
     rating = models.IntegerField()
-    author = User()
-    likes = [User]
+    author = models.ForeignKey(DjangoUser, null=False, on_delete=models.DO_NOTHING)
+    likes = models.ManyToManyField(DjangoUser)
 
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True)
     question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
-    author = User()
+    author = models.ForeignKey(DjangoUser, null=False, on_delete=models.DO_NOTHING)
