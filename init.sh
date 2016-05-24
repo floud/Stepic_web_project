767ed7441040b35d6531d45afec47098d5d90505
floud <ahalturin84@gmail.com>
2016-05-24 12:27:07 +0300

    еуые

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default1
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/test
-cd /home/box/web/ask
-sudo gunicorn -c /etc/gunicorn.d/test ask.wsgi:application &
+cd /home/box/web/ask/ask
+sudo gunicorn -c /etc/gunicorn.d/test wsgi:application &
 
-sudo mysqld_safe --user=mysql &
+sudo /etc/init.d/mysql start
 mysql -uroot -e "create database defaultdb;"
 mysql -uroot -e "CREATE USER 'floud' IDENTIFIED BY 'square';"
